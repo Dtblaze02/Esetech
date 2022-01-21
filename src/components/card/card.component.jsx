7bd0d2f2af60be0cd94f47b5652e90748d18a3fe
Dtblaze02 <44786869+Dtblaze02@users.noreply.github.com>
2022-01-21 15:54:53 +0100

    Update card.component.jsx

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -9,11 +9,12 @@ export const Card = (props) => (
     <div className="card mb-3">
   <div className="row no-gutters">
     <div className="col-md-4" style={{background: 'linear-gradient(#e66465, #9198e5)'}}> </div>
-    <div className="col-md-8">
+    <div className="col-md-8" style={{background: '#0e1a2b'}}>
       <div className="card-body">
-        <h5 className="card-title">{props.movie.name}</h5>
-        <p className="card-text"><small className="text-muted">Release Date: {props.movie.first_release_date}</small></p>
-        <p className="card-text">{truncate(props.movie.summary)}</p>        
+        <h5 className="card-title text-white">{props.movie.name}</h5>
+        <p className="card-text text-white"><small className="text-muted">Release Date: {props.movie.first_release_date}</small></p>
+        <p className="card-text text-muted">{truncate(props.movie.summary)}</p> 
+        <span className="text-white align-right">Rating: {props.movie.rating}</span>       
       </div>
     </div>
   </div>
