321b48f6a4a4b0e2b59a46dae03f10bf74013300
Dtblaze02 <44786869+Dtblaze02@users.noreply.github.com>
2022-01-24 16:15:10 +0100

    Update card.component.jsx

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -7,18 +7,17 @@ const truncate = (str) => {
 
 export const Card = (props) => (
     <div className="card mb-3">
-  <div className="row no-gutters">
-    <div className="col-md-4" style={{background: 'linear-gradient(#e66465, #9198e5)'}}> </div>
-    <div className="col-md-8" style={{background: '#0e1a2b'}}>
-      <div className="card-body">
-        <h5 className="card-title text-white">{props.movie.name}</h5>
-        <p className="card-text text-white"><small className="text-muted">Release Date: {props.movie.first_release_date}</small></p>
-        <p className="card-text text-muted">{truncate(props.movie.summary)}</p> 
-        <span className="text-white align-right">Rating: {props.movie.rating}</span>       
-      </div>
+        <div className="row no-gutters">
+            <div className="col-md-4" style={{background: 'linear-gradient(#e66465, #9198e5)'}}> </div>
+                <div className="col-md-8" style={{background: '#0e1a2b'}}>
+                    <div className="card-body">
+                        <h5 className="card-title text-white">{props.game.name}</h5>
+                        <p className="card-text text-white"><small className="text-muted">Release Date: {new Date(props.game.first_release_date).toLocaleDateString()}</small></p>
+                        <p className="card-text text-muted">{truncate(props.game.summary)}</p> 
+                        <span className="text-white align-right">Rating: {props.game.rating}</span>       
+                    </div>
+                </div>
+            </div>
     </div>
-  </div>
-</div>
-
 )
 
